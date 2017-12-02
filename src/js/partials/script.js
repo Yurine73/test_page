9892c98ef7b5947a13a6226c2be7fef1aa54bb01
Ксения <aozaki.toko@mail.ru>
2017-12-02 20:31:39 +0300

    Шапка + первый блок(без слайдера) + адаптив

--- src/js/partials/script.js
+++ src/js/partials/script.js
@@ -1,1 +1,14 @@
-$(function () {});
+var flag_menu;
+$(function () {
+	$(".burger").on("click", function(){
+		if(!flag_menu == 1) {
+			$(this).addClass("active");
+			$(".menu").addClass("open");
+			flag_menu = 1;
+		} else {
+			$(this).removeClass("active");
+			$(".menu").removeClass("open");
+			flag_menu = 0;
+		}
+	});
+});
